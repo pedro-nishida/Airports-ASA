b1f661175e5b942180082c8b2049d95bec7963a5
Pedro Nishida <phfujinami@gmail.com>
2025-05-07 00:51:58 -0300

    change password

--- models/database.py
+++ models/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:7355608@172.12.0.15:5432/companhia_aerea"
-
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@172.12.0.15:5432/companhia_aerea"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
